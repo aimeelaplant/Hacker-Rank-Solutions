e5a26746a8e105116c4d3834c86abf302e7d987c
Aimee LaPlant <alaplant@myfonts.com>
2016-04-28 17:20:38 -0400

    Create TypeOfTriangle.sql

---
+++ SQL/AdvancedSelect/TypeOfTriangle.sql
@@ -0,0 +1,9 @@
+/* https://www.hackerrank.com/challenges/what-type-of-triangle */
+SELECT 
+    CASE
+        WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+        WHEN A != B AND B != C THEN 'Scalene'
+    END 
+FROM TRIANGLES;
