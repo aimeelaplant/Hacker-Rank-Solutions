b6a34f87cf80275c8101785b5b52d698635c0b22
Aimee LaPlant <aimee@aimeelaplant.com>
2016-06-07 16:38:34 -0400

    Create Pangrams.java

---
+++ Algorithms/Strings/Pangrams.java
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/pangrams
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String sentence = new String(sc.nextLine().replace(" ", "").toLowerCase());
+        
+        // initialize hashset (no duplicates)
+        HashSet<Character> hashSet = new HashSet<Character>();
+        
+        
+        for(char character : sentence.toCharArray()) {
+            // add unique character to hashset
+            hashSet.add(character);
+        }
+        
+        // if hashset size is 26 then it is a pangram!
+        System.out.println((hashSet.size() == 26 ? "pangram" : "not pangram"));
+        
+    }
+}
