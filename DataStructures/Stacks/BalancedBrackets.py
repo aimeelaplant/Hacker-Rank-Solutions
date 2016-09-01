ffd68f9403ad10d5938b67e499290519f5bb032a
Aimee LaPlant <aimeelaplant@users.noreply.github.com>
2016-09-01 03:43:03 -0400

    Create BalancedBrackets.py

---
+++ DataStructures/Stacks/BalancedBrackets.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/balanced-brackets
+#!/bin/python
+
+import sys
+
+def is_balanced(string):
+    from collections import deque
+    d = deque()
+    for char in string:
+        if char == "{" or char == "(" or char == "[":
+            d.appendleft(char)
+            continue
+        elif char == "}":
+            if len(d) == 0 or d[0] != "{":
+                return "NO"
+            d.popleft()
+            continue
+        elif char == ")":
+            if len(d) == 0 or d[0] != "(":
+                return "NO"
+            d.popleft()
+            continue
+        elif char == "]":
+            if len(d) == 0 or d[0] != "[":
+                return "NO"
+            d.popleft()
+            continue
+    return "YES" if len(d) == 0 else "NO"
+
+t = int(raw_input().strip())
+for a0 in xrange(t):
+    s = raw_input().strip()
+    print is_balanced(s)
