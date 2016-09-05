7984f95070e6b07456a6de37bc720dee0d2d302d
Aimee LaPlant <aimeelaplant@users.noreply.github.com>
2016-09-05 15:01:46 -0400

    Update GameOfThronesI.py

--- Algorithms/Strings/GameOfThronesI.py
+++ Algorithms/Strings/GameOfThronesI.py
@@ -4,7 +4,7 @@ string = raw_input()
 found = False
 # Write the code to find the required palindrome and then assign the variable 'found' a value of True or False
 from collections import Counter
-c = Counter(map(lambda i:i, string))
+c = Counter(string)
 num_odd_chars = 0
 for w in c:
     # count odd occurrences
