095ac6b3629ca3c377a5823deacbe1e703f276ab
Aimee LaPlant <aimeelaplant@users.noreply.github.com>
2016-09-05 13:03:02 -0400

    Create GameOfThronesI.py

---
+++ Algorithms/Strings/GameOfThronesI.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/game-of-thrones
+string = raw_input()
+ 
+found = False
+# Write the code to find the required palindrome and then assign the variable 'found' a value of True or False
+from collections import Counter
+c = Counter(map(lambda i:i, string))
+num_odd_chars = 0
+for w in c:
+    # count odd occurrences
+    if c[w] % 2 != 0:
+        num_odd_chars += 1
+    if num_odd_chars > 1:
+        break # break out of loop so we aren't counting anymore
+if num_odd_chars > 1:
+    print("NO")
+else:
+    print("YES")
